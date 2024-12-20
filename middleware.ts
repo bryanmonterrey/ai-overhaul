874b1371d39dedaf9ecf752884c79bc20f56f576
bry0306 <37124301+bry0306@users.noreply.github.com>
2024-12-20 01:04:30 -0500

    jk

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import type { Database } from '@/supabase/functions/supabase.types';
-import { TokenChecker } from '@/app/lib/blockchain/token-checker';
+import { TokenChecker } from '../../lib/blockchain/token-checker';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
