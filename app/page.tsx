965776383af9291dc128cb31f0f83a689b0f2a8e
bry0306 <37124301+bry0306@users.noreply.github.com>
2024-12-25 22:36:41 -0500

    kjkj

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
                   springOptions={springOptions}
                   actionArea='global'
                   range={200}
-          
                 >
                <div className="border-zinc-900 font-medium hover:cursor-pointer border-2 bg-[#0D0E15]   hover:bg-[#00FFA2] hover:border-[#00FFA2] hover:text-[#11111A] transition-colors ease-in-out duration-300 rounded-lg p-4 text-white flex flex-col items-center">
                 <Link href="/chat">
